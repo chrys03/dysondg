3e631f5575dad6ff612df437c0a86b19d083a722
EC2 Default User <ec2-user@ip-172-31-34-39.us-east-2.compute.internal>
2020-02-21 19:12:36 +0000

    subs

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,10 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: access_token,
+  subscribed_fields: %w[feed mention name]
+)
 
 Bot.on :message do |message|
     message.reply(text: 'Hello, human!')
